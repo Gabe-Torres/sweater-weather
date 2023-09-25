294d3c49188ed6ae07f24030fe3a4050fdf255b8
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-25 15:59:54 -0500

    set up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :forecast, only: [:index] 
+      post '/registration', to: 'users#create'
     end
   end
 end
