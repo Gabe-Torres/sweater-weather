f9b09359c1b8385664bf2f6cae96e81b5143e386
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-24 22:14:03 -0500

    test for story 1

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Members Facade' do
+  context 'instance methods' do
+    scenario '#get_forecast' do
+      forcast = ForecastFacade.get_forecast('cincinnati,oh')
+
+      forcast.current_weather.each do |key, value|
+        expect(key).to eq(:last_updated).or eq(:temperature)
+                                        .or eq(:feels_like)
+                                        .or eq(:humidity)
+                                        .or eq(:visibility)
+                                        .or eq(:uvi)
+                                        .or eq(:condition)
+                                        .or eq(:icon)
+        expect(value).to be_a(String).or be_a(Float).or be_a(Integer)
+        expect(value).to_not be_a(Hash)
+      end
+
+      forcast.daily_weather.each do |day|
+        expect(day).to have_key(:date)
+        expect(day[:date]).to be_a(String)
+      end
+
+      forcast.hourly_weather.each do |hour|
+        expect(hour).to have_key(:time)
+        expect(hour[:time]).to be_a(String)
+  
+      end
+    end
+  end
+end
---
+++ spec/request/api/v0/forecast_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast', type: :request do
+  context 'GET /forecast' do
+    scenario 'returns forecast for a city' do 
+      location = 'cincinnati,oh'
+      get '/api/v0/forecast', params: { location: location }
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      expect(result[:data][:attributes][:current_weather]).to have_key(:last_updated)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:temperature)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:feels_like)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:humidity)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:visibility)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:uvi)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:condition)
+      expect(result[:data][:attributes][:current_weather]).to have_key(:icon)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:date)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:max_temp_f)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:min_temp_f)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:condition)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:icon)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:sunrise)
+      expect(result[:data][:attributes][:daily_weather][0]).to have_key(:sunset)
+      expect(result[:data][:attributes][:hourly_weather][0]).to have_key(:time)
+      expect(result[:data][:attributes][:hourly_weather][0]).to have_key(:temperature)
+      expect(result[:data][:attributes][:hourly_weather][0]).to have_key(:conditions)
+      expect(result[:data][:attributes][:hourly_weather][0]).to have_key(:icon)
+
+      expect(result[:data][:attributes][:current_weather]).to_not have_key(:feels_like_c)
+      expect(result[:data][:attributes][:current_weather]).to_not have_key(:temp_c)
+      expect(result[:data][:attributes][:current_weather]).to_not have_key(:wind_mph)
+
+      expect(result[:data][:attributes][:daily_weather][0]).to_not have_key(:date_epoch)
+      expect(result[:data][:attributes][:daily_weather][0]).to_not have_key(:avgtemp_c)
+      expect(result[:data][:attributes][:daily_weather][0]).to_not have_key(:totalsnow_cm)
+
+      expect(result[:data][:attributes][:hourly_weather][0]).to_not have_key(:time_epoch)
+      expect(result[:data][:attributes][:hourly_weather][0]).to_not have_key(:wind_mph)
+      expect(result[:data][:attributes][:hourly_weather][0]).to_not have_key(:pressure_in)
+    end
+  end
+end
