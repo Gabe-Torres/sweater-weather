c93f39971d882e6b6f9980261f96efd081e2edf9
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-25 22:20:20 -0500

    remove prys

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    # require 'pry'; binding.pry
     books = BookSearchFacade.book_search(params[:location], params[:quantity])
     forecast = ForecastFacade.get_forecast(params[:location]).current_weather
-    # binding.pry
     book_search = BookSearch.new(params[:location], forecast, books)
     render json: BookSearchSerializer.new(book_search).to_json, status: :ok
   end
