93321039a4efb3d0901c9e3e5f266c135207d04e
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-25 22:20:03 -0500

    refactor first attempt

--- spec/facades/book_search_facade_spec.rb
+++ spec/facades/book_search_facade_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe 'Book Search Facade', :vcr do
   context 'instance methods' do
     scenario 'book_search' do
       books = BookSearchFacade.book_search('denver,co', 5)
-      book = books[:books].first
-      expect(book).to be_a(Book)
-    
-      # expect(books.count).to eq(5)
-      expect(book).to be_an_instance_of(Book)
+
+      expect(books).to be_an(Array)
+      expect(books.count).to eq(5)
+      expect(books.first).to be_a(Book)
+      expect(books.first.title).to be_a(String)
+      expect(books.first.isbn).to be_an(Array)
+      expect(books.first.isbn.first).to be_a(String)
+      expect(books.first.title).to be_a(String)
     end
   end
 end
--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Book do
   it 'has attributes' do
     book = Book.new({isbn: '1234', title: 'The Great Gatsby'})
 
-    expect(book.books).to eq({isbn: '1234', title: 'The Great Gatsby'})
+    expect(book.isbn).to eq('1234')
+    expect(book.title).to eq('The Great Gatsby')
     expect(book).to be_a(Book)
   end
 end
