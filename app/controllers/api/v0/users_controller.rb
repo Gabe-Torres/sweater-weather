3cf137834b9537657b5659c3b7958a5f82b7a958
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-09-25 23:31:39 -0500

    remove pry

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    require 'pry'; binding.pry
     render json: UsersSerializer.new(User.create!(user_params)), status: :created
   rescue ActiveRecord::RecordInvalid => e
     render json: { error: e.message }, status: :bad_request
